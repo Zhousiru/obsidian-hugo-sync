1b2d95da612242ba8d529b3613e9d659bb684b38
Zhousiru <i@zhousiru.com>
2022-12-06 20:46:30 +0800

    feat(s3): put/get object

--- internal/s3/operate.go
+++ internal/s3/operate.go
@@ -1,10 +1,17 @@
 package s3
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"strings"
 
 	"github.com/minio/minio-go/v7"
+)
+
+const (
+	ContentTypeWebP = "image/webp"
+	ContentType
 )
 
 // List lists all objects with specific prefix.
@@ -32,6 +39,32 @@ func (c *Client) List(prefix string) ([]*Object, error) {
 		obj.ETag = object.ETag
 
 		ret = append(ret, obj)
+	}
+
+	return ret, nil
+}
+
+// Put uploads specified bytes object to bucket.
+func (c *Client) Put(objectKey string, object []byte, contentType string) error {
+	reader := bytes.NewReader(object)
+	_, err := c.client.PutObject(context.Background(), c.bucket, objectKey, reader, int64(len(object)), minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get downloads specified object.
+func (c *Client) Get(objectKey string) ([]byte, error) {
+	reader, err := c.client.GetObject(context.Background(), c.bucket, objectKey, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	ret, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
 	}
 
 	return ret, nil
