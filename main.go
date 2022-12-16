3404847e37c0b0837b001379806be7c720788b99
Zhousiru <i@zhousiru.com>
2022-12-16 18:49:44 +0800

    feat: posts sync

--- main.go
+++ main.go
@@ -1,35 +1,41 @@
 package main
 
 import (
+	"os"
 	"sync"
 
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/assetconv"
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/config"
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/logger"
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/mapping"
+	"github.com/Zhousiru/obsidian-hugo-sync/internal/postprocess"
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/s3"
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/util"
 )
 
 func main() {
 	vaultClient := new(s3.Client)
-	err := vaultClient.Setup(config.X.S3.Vault.Endpoint,
+	err := vaultClient.Setup(
+		config.X.S3.Vault.Endpoint,
 		config.X.S3.Vault.Region,
 		config.X.S3.Vault.AccessKeyID,
 		config.X.S3.Vault.SecretAccessKey,
-		config.X.S3.Vault.Bucket)
+		config.X.S3.Vault.Bucket,
+	)
 	if err != nil {
-		logger.Fatal("Failed to set up clients: %s", err)
+		logger.Fatal("Failed to set up vault bucket client: %s", err)
 	}
 
 	assetClient := new(s3.Client)
-	err = assetClient.Setup(config.X.S3.Asset.Endpoint,
+	err = assetClient.Setup(
+		config.X.S3.Asset.Endpoint,
 		config.X.S3.Asset.Region,
 		config.X.S3.Asset.AccessKeyID,
 		config.X.S3.Asset.SecretAccessKey,
-		config.X.S3.Asset.Bucket)
+		config.X.S3.Asset.Bucket,
+	)
 	if err != nil {
-		logger.Fatal("Failed to set up clients: %s", err)
+		logger.Fatal("Failed to set up asset bucket client: %s", err)
 	}
 
 	logger.Info("Load present asset mapping from local")
@@ -52,10 +58,10 @@ func main() {
 
 	logger.Info("Update assets bucket")
 
-	add, del := presentAsset.Diff(latestAsset)
+	assetAdd, assetDel := presentAsset.Diff(latestAsset)
 	var wg sync.WaitGroup
 
-	for _, ent := range del {
+	for _, ent := range assetDel {
 		logger.DelFile(ent.RawFilename, ent.Hash)
 		wg.Add(1)
 
@@ -74,7 +80,9 @@ func main() {
 		}(ent)
 	}
 
-	for _, ent := range add {
+	wg.Wait()
+
+	for _, ent := range assetAdd {
 		logger.NewFile(ent.RawFilename, ent.Hash)
 		wg.Add(1)
 
@@ -111,8 +119,88 @@ func main() {
 
 	err = presentAsset.Save()
 	if err != nil {
-		logger.Fatal("Failed to save mapping: %s", err)
+		logger.Fatal("Failed to save asset mapping: %s", err)
 	}
 
-	// TODO
+	presentPost := new(mapping.Mapping)
+	presentPost.SetType(mapping.PostMapping)
+	err = presentPost.Load()
+	if err != nil {
+		logger.Fatal("Failed to load present post mapping: %s", err)
+	}
+
+	logger.Info("Dump latest post mapping from S3")
+
+	postObjSlice, err := vaultClient.List(config.X.VaultPost)
+	if err != nil {
+		logger.Fatal("Failed to dump latest post mapping: %s", err)
+	}
+
+	latestPost := mapping.VaultToMapping(postObjSlice, config.X.VaultPost, mapping.PostMapping)
+
+	logger.Info("Update hugo posts")
+
+	postAdd, postDel := presentPost.Diff(latestPost)
+
+	for _, ent := range postDel {
+		logger.DelFile(ent.RawFilename, ent.Hash)
+		wg.Add(1)
+
+		go func(ent *mapping.Entry) {
+
+			defer wg.Done()
+
+			err := os.Remove(config.X.HugoPostPath + ent.ProcessedFilename)
+			if err != nil {
+				logger.Err("Failed to remove post %s: %s", ent.RawFilename, err)
+				return
+			}
+
+			presentPost.Remove(ent.Hash)
+
+		}(ent)
+	}
+
+	wg.Wait()
+
+	for _, ent := range postAdd {
+		logger.NewFile(ent.RawFilename, ent.Hash)
+		wg.Add(1)
+
+		go func(ent *mapping.Entry) {
+
+			defer wg.Done()
+
+			post, err := vaultClient.Get(config.X.VaultPost + ent.RawFilename)
+			if err != nil {
+				logger.Err("Failed to get post %s: %s", ent.RawFilename, err)
+				return
+			}
+
+			postStr := postprocess.Process(
+				string(post),
+				ent.RawFilename,
+				config.X.AssetUrl,
+				config.X.VaultAsset,
+			)
+
+			err = os.WriteFile(config.X.HugoPostPath+ent.ProcessedFilename, []byte(postStr), 0644)
+			if err != nil {
+				logger.Err("Failed to add post %s: %s", ent.RawFilename, err)
+				return
+			}
+
+			presentPost.Append(ent)
+
+		}(ent)
+	}
+
+	wg.Wait()
+
+	err = presentPost.Save()
+	if err != nil {
+		logger.Fatal("Failed to save post mapping: %s", err)
+	}
+
+	// TODO: hugo build
 }
