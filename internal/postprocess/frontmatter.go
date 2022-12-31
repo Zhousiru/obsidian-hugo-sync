3fdd5a911510f763cbcd7c08c1144394bf1fc4c9
Zhousiru <i@zhousiru.com>
2022-12-31 13:41:00 +0800

    refactor(postprocess): front matter workflow

--- internal/postprocess/frontmatter.go
+++ internal/postprocess/frontmatter.go
@@ -7,31 +7,36 @@ import (
 	"github.com/Zhousiru/obsidian-hugo-sync/internal/util"
 )
 
-// modifyTitle replaces `title: {auto}` to `title: <Post Filename Without Ext>`
-func modifyTitle(post, filename string) string {
-	title := util.TrimExt(filename)
-
+func modifyFrontMatter(post string, modifier ...func(string) string) string {
 	reFrontMatter := regexp.MustCompile(`^---\n[\S\s]*?\n---`)
 	ret := reFrontMatter.ReplaceAllStringFunc(post, func(frontMatter string) string {
-		reTitle := regexp.MustCompile(`title: .*?\n`)
-		return reTitle.ReplaceAllStringFunc(frontMatter, func(titleField string) string {
-			return strings.ReplaceAll(titleField, "{{auto}}", title)
-		})
+		modified := frontMatter
+		for _, m := range modifier {
+			modified = m(modified)
+		}
+		return modified
 	})
 
 	return ret
 }
 
-// modifyAssetRef replaces `{{asset image.jpg}}` to `https://<Asset URL>/image.webp`
-func modifyAssetRef(post, vaultAsset, assetUrl string) string {
-	reFrontMatter := regexp.MustCompile(`^---\n[\S\s]*?\n---`)
-	ret := reFrontMatter.ReplaceAllStringFunc(post, func(frontMatter string) string {
-		reAsset := regexp.MustCompile(`{{asset (.*?)}}`)
-		return reAsset.ReplaceAllStringFunc(frontMatter, func(assetFiedld string) string {
-			url := assetFiedld[8 : len(assetFiedld)-2]
-			return getImageUrl(url, vaultAsset, assetUrl)
-		})
-	})
+// titleModifier replaces `title: {auto}` to `title: <Post Filename Without Ext>`
+func titleModifier(frontMatter, filename string) string {
+	title := util.TrimExt(filename)
 
-	return ret
+	reTitle := regexp.MustCompile(`title: .*?\n`)
+
+	return reTitle.ReplaceAllStringFunc(frontMatter, func(titleField string) string {
+		return strings.ReplaceAll(titleField, "{{auto}}", title)
+	})
+}
+
+// assetRefModifier replaces `{{asset image.jpg}}` to `https://<Asset URL>/image.webp`
+func assetRefModifier(frontMatter, vaultAsset, assetUrl string) string {
+	reAsset := regexp.MustCompile(`{{asset (.*?)}}`)
+
+	return reAsset.ReplaceAllStringFunc(frontMatter, func(assetFiedld string) string {
+		url := assetFiedld[8 : len(assetFiedld)-2]
+		return getImageUrl(url, vaultAsset, assetUrl)
+	})
 }
--- internal/postprocess/process.go
+++ internal/postprocess/process.go
@@ -3,8 +3,15 @@ package postprocess
 const errStr = "[OBSIDIAN_HUGO_SYNC_ERROR]"
 
 func Process(post, filename, baseUrl, vaultPost, vaultAsset, assetUrl string) string {
-	post = modifyTitle(post, filename)
-	post = modifyAssetRef(post, vaultAsset, assetUrl)
+	post = modifyFrontMatter(
+		post,
+		func(s string) string {
+			return titleModifier(s, filename)
+		},
+		func(s string) string {
+			return assetRefModifier(s, vaultAsset, assetUrl)
+		},
+	)
 	post = convertImageMark(post, baseUrl, vaultAsset)
 	post = convertLinkMark(post, baseUrl, vaultPost)
 	post = convertWikilink(post, vaultPost, vaultAsset, baseUrl)
